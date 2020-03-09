048744bdf99d7154263ece3b7e0342f0f9d0c9e4
mvargasv95 <minorvargas46@gmail.com>
2020-03-09 00:16:22 -0600

    master: crud operations working as expected

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import { createClient } from 'redis'
+import { Types } from 'mongoose'
 import {
   get,
   post,
@@ -9,13 +11,14 @@ import {
   use
 } from './decorators'
 import Task from '../models/task.model'
-import { Types } from 'mongoose'
 
+const redis = createClient(6379, 'localhost')
+
+// middlewares
 const isIdValid = (req: Request, res: Response, next: NextFunction): void => {
   const { id } = req.params
-  console.log('here', id)
   if (Types.ObjectId.isValid(id)) return next()
-  res.status(400).send('Invalid Object ID')
+  res.status(400).send('Invalid task Id')
 }
 
 @controller('/')
@@ -26,6 +29,7 @@ class TaskController {
     const { id } = req.params
     try {
       const task = await Task.findById(id).exec()
+      if (!task) return res.status(400).send('Unable to find task')
       return res.status(200).json({ task })
     } catch (e) {
       console.error(e)
@@ -47,10 +51,36 @@ class TaskController {
   }
 
   @remove(':id')
-  removeTask(req: Request, res: Response) {}
+  @use(isIdValid)
+  async removeTask(req: Request, res: Response) {
+    const { id } = req.params
+    try {
+      const task = await Task.findByIdAndRemove(id)
+      if (!task) return res.status(404).send('Task not found')
+      return res.status(200).json({ task })
+    } catch (e) {
+      console.error(e)
+      return res.status(400).send('Bad request')
+    }
+  }
 
   @put(':id')
-  updateTask() {}
+  @use(isIdValid)
+  async updateTask(req: Request, res: Response) {
+    if (Object.keys(req.body).length === 0 && req.body.constructor === Object)
+      return res.status(400).send('No information to update')
+    const { id } = req.params
+    try {
+      const task = await Task.findByIdAndUpdate(id, req.body, {
+        new: true
+      }).exec()
+      if (!task) return res.status(400).send('Unable to update task')
+      return res.status(200).json({ task })
+    } catch (e) {
+      console.error(e)
+      return res.status(400).send('Bad request')
+    }
+  }
 
   @get('/')
   async getAll(req: Request, res: Response) {
@@ -59,10 +89,7 @@ class TaskController {
       return res.json({ tasks })
     } catch (e) {
       console.error(e)
-      return res
-        .status(400)
-        .send('Bad request')
-        .end()
+      return res.status(400).send('Bad request')
     }
   }
 }
--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -26,17 +26,23 @@ const status = [
 ]
 
 // tslint:disable-next-line: variable-name
-const TaskSchema: Schema = new Schema({
-  title: String,
-  description: String,
-  complexity: Number,
-  status: {
-    type: String,
-    enum: status
+const TaskSchema: Schema = new Schema(
+  {
+    title: String,
+    description: String,
+    complexity: Number,
+    status: {
+      type: String,
+      enum: status
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    modifiedAt: Date,
+    finishedAt: Date
   },
-  createdAt: Date,
-  modifiedAt: Date,
-  finishedAt: Date
-})
+  { timestamps: true }
+)
 
 export default mongoose.model<ITask>('Task', TaskSchema)
